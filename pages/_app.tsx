0ceba047b7e3265914c91f9821f575c0ddba93b1
Kevin Yu <kevinyu567@gmail.com>
2022-02-05 23:28:16 -0800

    Preliminary reformatting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />;
 }
-
-export default MyApp
