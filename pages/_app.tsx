3ce30c6f6cd4c72c6c558844d25d30de9727ca6b
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2022-02-12 17:17:33 -0800

    theme toggling and local saving

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,25 @@
 import {AppProps} from 'next/app';
 import '../styles/globals.css';
 import Head from "next/head";
-
+import { useCallback, useEffect } from 'react';
+import ThemeButton from '../components/themeButton';
 
 export default function MyApp({Component, pageProps}: AppProps) {
+    useEffect(()=>{
+        const darkMode = JSON.parse(''+localStorage.getItem('gunnelimination:darkMode'))
+        console.log(darkMode)
+        document.body.parentElement?.classList.toggle('dark', darkMode)
+    })
+    
+    const toggleTheme = useCallback(()=>{
+        const isDark = ''+document.body.parentElement?.classList.toggle('dark')
+        localStorage.setItem('gunnelimination:darkMode', isDark)
+    },[])
     return (
         <>
+        <button onClick={toggleTheme} className='absolute py-2 px-2 dark:hover:bg-black/20 rounded-lg hover:bg-white/20 top-2 right-2'>
+            <ThemeButton/>
+        </button>
             <Head>
                 <title>Gunn Elimination 2022</title>
             </Head>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,10 @@
 import Document, {Html, Head, Main, NextScript, DocumentContext} from 'next/document';
 
-
 class GDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
         const initialProps = await Document.getInitialProps(ctx);
         return { ...initialProps };
     }
-
     render() {
         return (
             <Html className="h-full dark">
