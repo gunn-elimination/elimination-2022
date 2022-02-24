e42ea55ee923714932eb62085f6e3a30a9042d8b
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2022-02-24 01:11:53 -0800

    dark mode announcements

--- pages/app/game/[game]/index.tsx
+++ pages/app/game/[game]/index.tsx
@@ -120,7 +120,7 @@ export default function Index() {
                         })
                     }
                 </div>
-                <div className={'rounded-md mb-2 bg-amber-500/20 text-amber-900 px-3 py-2 flex flex-row gap-2'}>
+                <div className={'rounded-md mb-2 bg-amber-500/20 text-amber-900 dark:text-amber-400 px-3 py-2 flex flex-row gap-2'}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-[20px] w-[20px] shrink-0 mt-1 mb-auto" viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M18 3a1 1 0 00-1.447-.894L8.763 6H5a3 3 0 000 6h.28l1.771 5.316A1 1 0 008 18h1a1 1 0 001-1v-4.382l6.553 3.276A1 1 0 0018 15V3z" clipRule="evenodd" />
                     </svg>
